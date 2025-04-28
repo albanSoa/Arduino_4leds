133e100c3751fd60d60645999cf9424aceb624a4
albanSoa <alban.soa@gmail.com>
2025-04-28 16:02:11 +0200

    4Leds.ino

--- 4Leds.ino
+++ 4Leds.ino
@@ -1,21 +1,22 @@
 // (c) Michael Schoeffler 2017, http://www.mschoeffler.de
 
-#include "Wire.h" // This library allows you to communicate with I2C devices.
+#include "Wire.h" // Cette librairie permet la communication avec les périphériques I2C.
 
-const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
+const int MPU_ADDR = 0x68; // Adresse I2C du MPU-6050. Si la broche AD0 est à HIGH, l'adresse devient 0x69.
 
-int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
-int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
-int16_t temperature; // variables for temperature data
+int16_t accelerometre_x, accelerometre_y, accelerometre_z; // Variables pour les données brutes de l'accéléromètre
+int16_t gyroscope_x, gyroscope_y, gyroscope_z; // Variables pour les données brutes du gyroscope
+int16_t temperature; // Variable pour la température
 
-#define LED_LB 2
-#define LED_RB 3
-#define LED_RT 4
-#define LED_LT 5
+#define LED_LB 2 // LED en bas à gauche
+#define LED_RB 3 // LED en bas à droite
+#define LED_RT 4 // LED en haut à droite
+#define LED_LT 5 // LED en haut à gauche
 
-char tmp_str[7]; // temporary variable used in convert function
+char tmp_str[7]; // Variable temporaire utilisée pour convertir les entiers en chaînes
 
-char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
+// Fonction de conversion d'un int16_t en chaîne de caractères
+char* convertir_int16_en_str(int16_t i) {
   sprintf(tmp_str, "%6d", i);
   return tmp_str;
 }
@@ -30,64 +31,70 @@ void setup() {
   digitalWrite(LED_RB, LOW);
   digitalWrite(LED_RT, LOW);
   digitalWrite(LED_LT, LOW);
+  
   Wire.begin();
-  Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
-  Wire.write(0x6B); // PWR_MGMT_1 register
-  Wire.write(0); // set to zero (wakes up the MPU-6050)
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x6B); // Registre PWR_MGMT_1
+  Wire.write(0); // Mettre à zéro pour sortir du mode veille
   Wire.endTransmission(true);
 }
+
 void loop() {
   Wire.beginTransmission(MPU_ADDR);
-  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
-  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
-  Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
+  Wire.write(0x3B); // Commencer à lire à partir du registre 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_ADDR, 7 * 2, true); // Demander 14 octets (7 valeurs de 2 octets)
+
+  // Lecture des valeurs de l'accéléromètre
+  accelerometre_x = Wire.read() << 8 | Wire.read();
+  accelerometre_y = Wire.read() << 8 | Wire.read();
+  accelerometre_z = Wire.read() << 8 | Wire.read();
   
-  // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
-  accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
-  accelerometer_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
-  accelerometer_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
-  temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
-  gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
-  gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
-  gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+  // Lecture de la température
+  temperature = Wire.read() << 8 | Wire.read();
   
-  // print out data
-  Serial.print("aX = "); Serial.print(convert_int16_to_str(accelerometer_x));
-  Serial.print(" | aY = "); Serial.print(convert_int16_to_str(accelerometer_y));
-  Serial.print(" | aZ = "); Serial.print(convert_int16_to_str(accelerometer_z));
-  // the following equation was taken from the documentation [MPU-6000/MPU-6050 Register Map and Description, p.30]
-  Serial.print(" | tmp = "); Serial.print(temperature/340.00+36.53);
-  Serial.print(" | gX = "); Serial.print(convert_int16_to_str(gyro_x));
-  Serial.print(" | gY = "); Serial.print(convert_int16_to_str(gyro_y));
-  Serial.print(" | gZ = "); Serial.print(convert_int16_to_str(gyro_z));
+  // Lecture des valeurs du gyroscope
+  gyroscope_x = Wire.read() << 8 | Wire.read();
+  gyroscope_y = Wire.read() << 8 | Wire.read();
+  gyroscope_z = Wire.read() << 8 | Wire.read();
+
+  // Affichage des données sur le moniteur série
+  Serial.print("aX = "); Serial.print(convertir_int16_en_str(accelerometre_x));
+  Serial.print(" | aY = "); Serial.print(convertir_int16_en_str(accelerometre_y));
+  Serial.print(" | aZ = "); Serial.print(convertir_int16_en_str(accelerometre_z));
+  Serial.print(" | Temp = "); Serial.print(temperature / 340.00 + 36.53);
+  Serial.print(" | gX = "); Serial.print(convertir_int16_en_str(gyroscope_x));
+  Serial.print(" | gY = "); Serial.print(convertir_int16_en_str(gyroscope_y));
+  Serial.print(" | gZ = "); Serial.print(convertir_int16_en_str(gyroscope_z));
   Serial.println();
 
-  if (accelerometer_x < 500 && accelerometer_y < -4000) {
+  // Logique d'allumage des LEDs en fonction des valeurs de l'accéléromètre
+  if (accelerometre_x < 500 && accelerometre_y < -4000) {
     digitalWrite(LED_LB, HIGH);
     digitalWrite(LED_RB, HIGH);
     digitalWrite(LED_RT, LOW);
     digitalWrite(LED_LT, LOW);
-  }else if (accelerometer_y > 4000) {
+  } else if (accelerometre_y > 4000) {
     digitalWrite(LED_LB, LOW);
     digitalWrite(LED_RB, LOW);
     digitalWrite(LED_RT, HIGH);
     digitalWrite(LED_LT, HIGH);  
-  }else if (accelerometer_x > 4000) {
+  } else if (accelerometre_x > 4000) {
     digitalWrite(LED_LB, LOW);
     digitalWrite(LED_RB, HIGH);
     digitalWrite(LED_RT, HIGH);
     digitalWrite(LED_LT, LOW); 
-  }else if (accelerometer_x < -4000 && accelerometer_y < 500) {
+  } else if (accelerometre_x < -4000 && accelerometre_y < 500) {
     digitalWrite(LED_LB, HIGH);
     digitalWrite(LED_RB, LOW);
     digitalWrite(LED_RT, LOW);
     digitalWrite(LED_LT, HIGH);
-  }else{
-   digitalWrite(LED_LB, LOW);
-   digitalWrite(LED_RB, LOW);
-   digitalWrite(LED_RT, LOW);
-   digitalWrite(LED_LT, LOW); 
+  } else {
+    digitalWrite(LED_LB, LOW);
+    digitalWrite(LED_RB, LOW);
+    digitalWrite(LED_RT, LOW);
+    digitalWrite(LED_LT, LOW);
   }
-  //delay
-  delay(10);
+
+  delay(10); // Petite pause pour éviter un rafraîchissement trop rapide
 }
